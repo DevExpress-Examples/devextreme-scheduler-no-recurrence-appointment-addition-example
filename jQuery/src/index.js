02f5f1c483fc68c9cadef1ebb954d2c49a9dd9d6
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2023-02-15 13:45:11 +0400

    removed cancelAppointmentAdding from jQuery example

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -19,83 +19,81 @@ $(() => {
             handleAppointmentUpdate(e, popup);
         }
     })
-});
 
-const popup = $('#popup').dxPopup({
-    contentTemplate: () => $('<div>').append(
-        $(`<p>There is a recurrent appointment in this cell.</p>`),
-    ),
-    width: 300,
-    height: 150,
-    container: '.dx-viewport',
-    showTitle: true,
-    title: 'Information',
-    visible: false,
-    dragEnabled: false,
-    hideOnOutsideClick: true,
-    showCloseButton: false,
-    position: {
-        at: 'center',
-        my: 'center',
-    },
-    toolbarItems: [{
-        widget: 'dxButton',
-        toolbar: 'bottom',
-        location: 'after',
-        options: {
-            text: 'Close',
-            onClick() {
-                popup.hide();
-            },
+    const popup = $('#popup').dxPopup({
+        contentTemplate: () => $('<div>').append(
+            $(`<p>There is a recurrent appointment in this cell.</p>`),
+        ),
+        width: 300,
+        height: 150,
+        container: '.dx-viewport',
+        showTitle: true,
+        title: 'Information',
+        visible: false,
+        dragEnabled: false,
+        hideOnOutsideClick: true,
+        showCloseButton: false,
+        position: {
+            at: 'center',
+            my: 'center',
         },
-    }],
-}).dxPopup('instance');
+        toolbarItems: [{
+            widget: 'dxButton',
+            toolbar: 'bottom',
+            location: 'after',
+            options: {
+                text: 'Close',
+                onClick() {
+                    popup.hide();
+                },
+            },
+        }],
+    }).dxPopup('instance');
 
-const handleAppointmentAdd = (event, popup) => {
-    handleAppointmentActions(
-        event,
-        getRecurrentAppointments(),
-        event.appointmentData,
-        popup
-    );
-}
-const handleAppointmentUpdate = (event, popup) => {
-    const recurrentAppointments = getRecurrentAppointments()
-        .filter((appointment) => appointment !== event.oldData);
-    handleAppointmentActions(
+    const handleAppointmentAdd = (event, popup) => {
+        handleAppointmentActions(
+            event,
+            getRecurrentAppointments(),
+            event.appointmentData,
+            popup
+        );
+    }
+
+    const handleAppointmentUpdate = (event, popup) => {
+        const recurrentAppointments = getRecurrentAppointments()
+            .filter((appointment) => appointment !== event.oldData);
+        handleAppointmentActions(
+            event,
+            recurrentAppointments,
+            event.newData,
+            popup
+        )
+    }
+
+    const handleAppointmentActions = (
         event,
         recurrentAppointments,
-        event.newData,
-        popup
-    )
-}
-const handleAppointmentActions = (
-    event,
-    recurrentAppointments,
-    newAppointment,
-    popup) => {
-    for (const recurrentAppointment of recurrentAppointments) {
-        const isOverlap = isOverlapRecurrentAppointment(
-            event,
-            recurrentAppointment,
-            newAppointment);
-        if (isOverlap) {
-            cancelAppointmentAdding(event, popup);
+        newAppointment,
+        popup) => {
+        for (const recurrentAppointment of recurrentAppointments) {
+            const isOverlap = isOverlapRecurrentAppointment(
+                event,
+                recurrentAppointment,
+                newAppointment);
+            if (isOverlap) {
+                event.cancel = true;
+                popup.show();
+            }
         }
     }
-}
 
-const getRecurrentAppointments = () => {
-    return defaultData
-        .filter((appointment) => appointment?.recurrenceRule)
-        .map((appointment) => ({
-            ...appointment,
-            startDate: new Date(appointment.startDate),
-            endDate: new Date(appointment.endDate),
-        }));
-}
-
-const cancelAppointmentAdding = (event, popup) => {
-    event.cancel = true;
-    popup.show();
-}
+    const getRecurrentAppointments = () => {
+        return defaultData
+            .filter((appointment) => appointment?.recurrenceRule)
+            .map((appointment) => ({
+                ...appointment,
+                startDate: new Date(appointment.startDate),
+                endDate: new Date(appointment.endDate),
+            }));
+    }
+});
